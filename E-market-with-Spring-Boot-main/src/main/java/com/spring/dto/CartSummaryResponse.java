530152c97500fc885e626eb2436ce604b873fe87
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 23:12:20 +0600

    Update CartSummaryResponse.java

--- E-market-with-Spring-Boot-main/src/main/java/com/spring/dto/CartSummaryResponse.java
+++ E-market-with-Spring-Boot-main/src/main/java/com/spring/dto/CartSummaryResponse.java
@@ -1,19 +1,3 @@
-//package com.spring.dto;
-//
-//import lombok.Builder;
-//import lombok.Data;
-//
-//import java.math.BigDecimal;
-//import java.util.List;
-//
-//@Data
-//@Builder
-//public class CartSummaryResponse {
-//    private String userCode;
-//    private int totalItems;
-//    private BigDecimal totalPrice;
-//    private List<CartItemResponse> items;
-//}
 package com.spring.dto;
 
 import lombok.Builder;
