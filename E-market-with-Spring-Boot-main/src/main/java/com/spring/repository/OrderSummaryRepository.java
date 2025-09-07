d37814adfdb2bb87c454082b3bea9a291dcc0a37
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 23:21:17 +0600

    Update OrderSummaryRepository.java

--- E-market-with-Spring-Boot-main/src/main/java/com/spring/repository/OrderSummaryRepository.java
+++ E-market-with-Spring-Boot-main/src/main/java/com/spring/repository/OrderSummaryRepository.java
@@ -1,12 +1,3 @@
-//package com.spring.repository;
-//
-//import com.spring.model.OrderSummary;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface OrderSummaryRepository extends JpaRepository<OrderSummary, Long> {
-//}
 package com.spring.repository;
 
 import com.spring.model.OrderSummary;
