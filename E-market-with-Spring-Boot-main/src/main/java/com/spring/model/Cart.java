f5e3c1179d7217f239aceafd09daf4b020a77f0c
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 23:14:39 +0600

    Update Cart.java

--- E-market-with-Spring-Boot-main/src/main/java/com/spring/model/Cart.java
+++ E-market-with-Spring-Boot-main/src/main/java/com/spring/model/Cart.java
@@ -1,27 +1,3 @@
-//package com.spring.model;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//import java.util.List;
-//
-//@Entity
-//@Table(name = "carts")
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class Cart {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String userCode; 
-//
-//    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<CartItem> items;
-//}
 package com.spring.model;
 
 import jakarta.persistence.*;
