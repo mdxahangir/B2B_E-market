7fe3794043760268738cf25e1798426c4402d0a2
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 23:20:51 +0600

    Update CartRepository.java

--- E-market-with-Spring-Boot-main/src/main/java/com/spring/repository/CartRepository.java
+++ E-market-with-Spring-Boot-main/src/main/java/com/spring/repository/CartRepository.java
@@ -1,12 +1,3 @@
-//package com.spring.repository;
-//
-//import com.spring.model.Cart;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import java.util.Optional;
-//
-//public interface CartRepository extends JpaRepository<Cart, Long> {
-//    Optional<Cart> findByUserCode(String userCode);
-//}
 package com.spring.repository;
 
 import com.spring.model.Cart;
