49e7aa998537c7e23f3d4c94dfb7e841112b37cd
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 23:15:06 +0600

    Update CartItem.java

--- E-market-with-Spring-Boot-main/src/main/java/com/spring/model/CartItem.java
+++ E-market-with-Spring-Boot-main/src/main/java/com/spring/model/CartItem.java
@@ -1,32 +1,3 @@
-//package com.spring.model;
-//
-//import java.math.BigDecimal;
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Entity
-//@Table(name = "cart_items")
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class CartItem {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Product product;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    private Cart cart;
-//
-//    private int quantity;
-//
-//    private BigDecimal price;
-//
-//}
 package com.spring.model;
 
 import jakarta.persistence.*;
