486a6526adcc03adb102c6ab773c082efbb6209f
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-10-18 12:26:38 +0600

    Clean up category_service.dart by removing comments
    Removed commented-out code for loading categories.

--- E-market-with-Flutter-main/lib/services/category_service.dart
+++ E-market-with-Flutter-main/lib/services/category_service.dart
@@ -43,7 +43,5 @@ class CategoryService {
       throw Exception('Failed to delete category');
     }
   }
-
-  // For loading categories list (alias)
   static Future<List<Category>> getAllCategories() => fetchCategories();
 }
